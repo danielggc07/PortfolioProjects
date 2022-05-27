8ae2872c3c6c2538801750ca2c8b8b93ab605f35
Daniel Guevara <104793892+danielggc07@users.noreply.github.com>
2022-05-26 23:35:44 -0400

    Update Challenge: Karaoke song selector.sql

--- KhanAcademyChallenges/Challenge: Karaoke song selector.sql
+++ KhanAcademyChallenges/Challenge: Karaoke song selector.sql
@@ -1,3 +1,8 @@
+/*
+Karaoke song selector
+Skills used: AND, OR
+*/
+
 -- Step 1 Ever sung karaoke? It's a place where you sing songs with your friends, and it's a lot of fun. We've created a table with songs, 
 -- and in this challenge, you'll use queries to decide what songs to sing. For the first step, select all the song titles.
 
